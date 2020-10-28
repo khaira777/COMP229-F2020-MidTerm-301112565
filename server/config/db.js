44651287e275da0cdbb273ff150ae5de46ece0b6
Gurkirat Khaira <777gurkirat@gmail.com>
2020-10-28 15:50:57 -0400

    updated mongodb atlas database URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://khaira:k55B9yEbExEvh2am@midterm.rqrwv.mongodb.net/booklist?retryWrites=true&w=majority"
+  //k55B9yEbExEvh2am
 };
