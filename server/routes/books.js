f5541bfa0dc06743d2431c554470677dc622b6f4
Gurkirat Khaira <777gurkirat@gmail.com>
2020-10-30 17:01:14 -0400

    made some changes for better funcitioning

--- server/routes/books.js
+++ server/routes/books.js
@@ -49,7 +49,7 @@ router.post('/add', (req, res, next) => {
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/edit/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
     let id = req.params.id;
 
     book.findById(id, (err, bookToUpdate) => {
@@ -65,7 +65,7 @@ router.get('/edit/:id', (req, res, next) => {
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/edit/:id', (req, res, next) => {
+router.post('/:id', (req, res, next) => {
     let id = req.params.id
     
     let updatedBook = book({
